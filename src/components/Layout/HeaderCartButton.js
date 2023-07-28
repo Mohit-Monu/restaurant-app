a661d354947356c207650d163317d8c76f5e72f6
mohitr Agarwal <bmohit700@gmail.com>
2023-07-28 19:26:17 +0530

    first commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+function HeaderCartButton() {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon/>
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>5</span>
+    </button>
+  );
+}
+export default HeaderCartButton;
